bd8c2953495ecd01f7a726d867c1bf84a6d03303
shredia <36351492+shredia@users.noreply.github.com>
2025-05-27 17:27:49 -0400

    se configuró el PLL para la transformada de park

---
+++ observabilidad_y_controlabilidad.m
@@ -0,0 +1,12 @@
+
+R = 
+A = [-R/L 0 km*W*sin(tetha) 0;
+     0 -R/L -Km*W*cos(tetha) 0;
+     -Km*W*sin(tetha)/J Km*W*cos(tetha)/J -B/J 0;
+      0 0 1 0];
+B = [1/L 1/L 0 0];
+
+C = [0 0 1 0;0 0 0 1];
+
+
+D = [0 0];
