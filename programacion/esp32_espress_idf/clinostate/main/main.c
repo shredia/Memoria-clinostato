992263af67fac50ff0d83b9531f89bbcbd18d21f
shredia <36351492+shredia@users.noreply.github.com>
2025-08-03 21:48:51 -0400

    Update AccelStepper

--- programacion/esp32_espress_idf/clinostate/main/main.c
+++ programacion/esp32_espress_idf/clinostate/main/main.c
@@ -2,10 +2,6 @@
 #include "motores.h"
 #include <driver/gpio.h>
 
-#include "LP8_protocolo.h" //los distintos protocolos a llamar para trabajar con LP8 mediante las funcione
-
-
-
 void app_main() {
  
 }
