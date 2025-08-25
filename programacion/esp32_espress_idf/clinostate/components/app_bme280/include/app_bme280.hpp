5193dde5a59587af70dad98a2720a84a1fbca5ba
shredia <36351492+shredia@users.noreply.github.com>
2025-08-25 17:12:54 -0400

    Revert "ónLP8: Auto stash before merge of "resurrecciónLP8" and "Developer""
    This reverts commit 09935a00c613aaedba4bdea5304b57de603cbf36.

--- programacion/esp32_espress_idf/clinostate/components/app_bme280/include/app_bme280.hpp
+++ programacion/esp32_espress_idf/clinostate/components/app_bme280/include/app_bme280.hpp
@@ -1,8 +1,1 @@
-#pragma once
-#include "i2c_bus.h"
-#include "bme280.h"
-extern bool continuousMeasurement;
-void bme280_setup();
-void bme280_leer(float &temperature, float &humidity, float &pressure);
 void bme280_start_periodic();
-void bme280_task(void *pvParameters);
